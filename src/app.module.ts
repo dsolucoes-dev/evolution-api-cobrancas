51e4274533803d7e340651d9361e31c7c644f4fa
Carlos David <romao.carlosdavi@gmail.com>
2025-03-22 12:11:59 -0300

    feat: adiciona módulo API com controller e service; inclui DTOs para criação e atualização

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [],
+  imports: [ApiModule],
   controllers: [],
   providers: [],
 })
